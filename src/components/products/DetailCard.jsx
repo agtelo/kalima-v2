fcd4cbd7614a39870918bb181350751f7b0e6080
agtelo <alantelo1987@gmail.com>
2024-09-07 20:21:14 -0300

    FIX: new changes 5

--- src/components/products/DetailCard.jsx
+++ src/components/products/DetailCard.jsx
@@ -30,7 +30,7 @@ export default function DetailCard() {
 	}
 
 	if (error) return <div> {error.message} </div>
-	if (isLoading) return
+	if (isLoading) return <Spinner />
 	if (!data || data.length === 0) return <div>No featured products available.</div> //
 	return (
 		<div className='flex flex-col max-w-6xl gap-8 p-4 mx-auto md:flex-row md:p-8 lg:p-20'>
